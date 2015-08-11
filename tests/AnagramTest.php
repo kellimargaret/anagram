493e3369c49cfafd2e33047af3a4283b95f8c0a3
Kelli Margaret & Austin Blanchard <student@epicodus.com>
2015-08-11 10:49:41 -0700

    Add full word to input test

--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -5,17 +5,17 @@
     class AnagramTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_makeAnagram_firstLetter()
+        function test_makeAnagram_firstWord()
         {
             //Arrange
             $test_Anagram = new Anagram;
-            $input = "B";
+            $input = "Banana";
 
             //Act
             $result = $test_Anagram->makeAnagram($input);
 
             //Assert
-            $this->assertEquals("B", $result);
+            $this->assertEquals("Banana", $result);
         }
     }
 
