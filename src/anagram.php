8267d57c910cf9fce0764e68997ebcdd8f7422e7
Kelli Margaret & Austin Blanchard <student@epicodus.com>
2015-08-11 11:32:02 -0700

    Add input array to explode string

--- src/anagram.php
+++ src/anagram.php
@@ -4,8 +4,16 @@ class Anagram
 {
     function makeAnagram($input_anagram)
     {
-        return strtolower($input_anagram);
+        $input_array_of_words = explode(", ", $input_anagram);
+        $output_makeanagram = array();
+        foreach ($input_array_of_words as $word) {
+            array_push($output_makeanagram, strtolower($word));
+        }
+        return implode(", ", $output_makeanagram);
     }
 }
+
+
+
 
 ?>
